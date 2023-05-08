106b725a23069779d6261b1d11e37fd918e0e352
boma086 <boma086@gmail.com>
2023-05-08 18:24:51 +0800

    commit

---
+++ src/main/java/com/example/demo/FirstJUnit5Tests.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class FirstJUnit5Tests {
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1+1);
+    }
+}
